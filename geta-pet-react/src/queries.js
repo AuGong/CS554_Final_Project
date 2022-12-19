da60a6af8acda07d66e98ec2a36bf511c03c0940
Guangwei Na <gna@stevens.edu>
2022-12-19 16:13:44 -0500

    Updatae react queries

--- geta-pet-react/src/queries.js
+++ geta-pet-react/src/queries.js
@@ -32,9 +32,61 @@ const GET_PET_LIST = gql`
   }
 `;
 
+const GET_POST_PETS = gql`
+  query postPets(
+    $userId: String!
+  ){
+    getPostPets(userId: $userId){
+      id
+      name
+      breed
+      age
+      gender
+      size
+      description
+      contact
+      photos {
+        small
+        medium
+        large
+        full
+      }
+      liked
+    }
+  }
+  `;
+  const GET_LIKE_LIST = gql`
+  query likeList(
+    $userId: String!
+  ){
+    getLikes(userId: $userId){
+      id
+      name
+      breed
+      age
+      gender
+      size
+      description
+      contact
+      photos {
+        small
+        medium
+        large
+        full
+      }
+      liked
+    }
+  }
+  `;
+
 const UPLOAD_LIKE = gql`
   mutation uploadLike($symbol: String, $userId: String, $petId: String) {
     updateLike(symbol: $symbol, userId: $userId, petId: $petId)
+  }
+`;
+const POST_PET = gql`
+  mutation petPost($image: String, $name: String, $breed:String, $description: String, $age:Int, $size:String, $gender:String, $contact:String){
+    postPet(image: $image, name:$name, breed: $breed, description:$description, age:$age, size:$size, gender:$gender, contact:$contact)
   }
 `;
 
@@ -42,6 +94,9 @@ const UPLOAD_LIKE = gql`
 let exported = {
   GET_PET_LIST,
   UPLOAD_LIKE,
+  POST_PET,
+  GET_POST_PETS,
+  GET_LIKE_LIST,
 };
 
 export default exported;
