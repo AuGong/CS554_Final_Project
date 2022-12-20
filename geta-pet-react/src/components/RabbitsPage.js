6b1b9f61af0202548be6b9d767cbf396652c91c8
mickylab <42708694+mickylab@users.noreply.github.com>
2022-12-20 13:03:50 -0500

    change h1 name

--- geta-pet-react/src/components/RabbitsPage.js
+++ geta-pet-react/src/components/RabbitsPage.js
@@ -51,7 +51,7 @@ const RabbitsPage = () => {
   if (data) {
     return (
       <div>
-        <h1>Dog Buddies</h1>
+        <h1>Rabbit Buddies</h1>
         <div className="row g-3 align-items-center">
           <div className="col-auto">
             <label htmlFor="inputLocation" className="col-form-label">
